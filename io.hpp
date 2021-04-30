f1089817b540cc47d769a32f312267e0cd6958f0
Dmitriy <dima.gladkikh.2016@mail.ru>
2021-04-30 22:05:06 +0700

    Dz8

---
+++ io.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <iostream>
+
+#include "logic.hpp"
+
+void print_greeting()
+{
+    std::cout << "Welcome to Tic Tac Toe game!\n";
+}
+
+Pos query_player_move(GameField const &field)
+{
+    Pos pos;
+    std::cout << "Please make a move: input two number in range [1-3]\n";
+    bool ok = false;
+
+    while (!ok)
+    {
+        std::cout << "> ";
+        std::cin >> pos.x >> pos.y;
+        pos.x--;
+        pos.y--;
+        if (!std::cin)
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max());
+            std::cout << "Error: please input numbers!\n";
+        }
+        else if (!is_valid_pos(pos.x, pos.y))
+        {
+            std::cout << "Error: position is out of field bounds!\n";
+        }
+        else if (!pusto(field, pos.x, pos.y))
+        {
+            std::cout << "Error: position is occupied!\n";
+        }
+        else
+        {
+            ok = true;
+        }
+    }
+    return pos;
+}
+
+void print_cell(CellState cell)
+{
+    switch (cell)
+    {
+    case CellState::Empty:
+        std::cout << ".";
+        break;
+
+    case CellState::X:
+        std::cout << "X";
+        break;
+
+    case CellState::O:
+        std::cout << "O";
+        break;
+    }
+}
+
+void print_field(GameField const &field)
+{
+    std::cout << "=======\n";
+    for (size_t col = 0; col < FIELD_WIDTH; col++)
+    {
+        std::cout << "||";
+        for (size_t row = 0; row < FIELD_WIDTH; row++)
+        {
+            print_cell(get_cell(field, row, col));
+        }
+        std::cout << "||\n";
+    }
+    std::cout << "=======\n";
+}
+
+void print_game_outcome(TurnOutcome outcome) {
+    switch (outcome)
+    {
+    case TurnOutcome::DRAW:
+        std::cout << "It's a draw!\n";
+        break;
+    
+    case TurnOutcome::X_WON:
+        std::cout << "You won! Congrats!\n";
+        break;
+
+    case TurnOutcome::O_WON:
+        std::cout << "You lost. Good luck next time.\n";
+        break;
+
+    default:
+        break;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cassert>
+
+#include "logic.hpp"
+#include "io.hpp"
+#include "ai.hpp"
+
+int main() {
+    // Human is always X
+    // AI is always O
+
+    // while game is not over
+    //  - read player input / run AI
+    //  - apply move
+    //  - process new game state
+    //  - print turn outcome
+    print_greeting();
+
+    GameField field {};
+    TurnOutcome outcome {};
+    PlayerSign current_player = PlayerSign::X;
+    do {
+        Pos pos;
+        if (current_player == PlayerSign::X) {
+            pos = query_player_move(field);
+        } else {
+            pos = query_ai_move(field);
+        }
+        assert(set_cell(field, pos.x, pos.y, sign_to_cell(current_player)));
+
+        outcome = check_turn_outcome(field);
+        print_field(field);
+        current_player = next_player(current_player);
+
+    } while (outcome == TurnOutcome::CONTINUE);
+
+    print_game_outcome(outcome);
+
+    return 0;
+}
